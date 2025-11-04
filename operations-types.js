6915c31d315b1df4b2613720978f2cd1161facbb
Yvvonne2 <yvvonnemthiyane@gmail.com>
2025-11-04 10:14:06 +0200

    first commit

---
+++ operations-types.js
@@ -0,0 +1,24 @@
+// TODO: Create a variable called number1 and store any number in it
+const number1 = 10;
+
+// TODO: Create a variable called number2 and store a different number in it
+const number2 = 4;
+
+// TODO: Convert number1 to a string and store it in a new variable
+const number1AsString = String(number1);
+
+// TODO: Add number1 and number2 and store the result in a variable called sum
+const sum = number1 + number2;
+
+// TODO: Multiply number1 and number2 and store the result in a variable called product
+const product = number1 * number2;
+
+// TODO: Print all results to the console
+console.log("Number 1:", number1);
+console.log("Number 2:", number2);
+console.log("Number 1 as a string:", number1AsString);
+console.log("Sum of number1 and number2:", sum);
+console.log("Product of number1 and number2:", product);
+
+// Optional: show how JavaScript handles mixed types
+console.log("Adding a number and a string:", number1 + number1AsString);
